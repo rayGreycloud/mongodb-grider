7723a04d1d2f4c64eb5a42eb3cfcffc86cdca2d9
rayGreycloud <rayGreycloud@gmail.com>
2017-02-02 08:57:10 -0800

    Add test of skip and limit with find

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,15 +2,24 @@ const assert = require('assert');
 const User = require('../src/user')
 
 describe('Methods to read records', () => {
-  let joe;
+  let joe, kit, liz, moe;
 
   beforeEach((done) => {
     joe = new User({ name: 'Joe' });
-    joe.save()
+    kit = new User({ name: 'Kit' });
+    liz = new User({ name: 'Liz' });
+    moe = new User({ name: 'Moe' });
+
+    Promise.all([
+      joe.save(),
+      kit.save(),
+      liz.save(),
+      moe.save()
+    ])
       .then(() => done());
   });
 
-  it('class method find', (done) => {
+  it('should use class method find', (done) => {
     User.find({ name: 'Joe' })
       .then((users) => {
         // Convert ObjectId to string
@@ -19,11 +28,24 @@ describe('Methods to read records', () => {
       });
   });
 
-  it('class method findOne', (done) => {
+  it('should use class method findOne', (done) => {
     User.findOne({ _id: joe._id })
       .then((user) => {
         assert(user.name === 'Joe');
         done();
       })
+  });
+
+  it('should use modifiers skip and limit', (done) => {
+    User.find({}) // find all records
+      .skip(1)  // skip 1st records
+      .limit(2) // limit results to 2
+      .then((users) => {
+        console.log(users[0]);
+        assert(users[0].name === 'Kit');
+        assert(users[1].name === 'Liz');
+        assert(users.length === 2);
+        done();
+      });
   });
 });
