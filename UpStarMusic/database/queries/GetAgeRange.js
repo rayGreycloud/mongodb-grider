427839bd8afb6b1664b9e5b8de644bd3eb5b69e8
rayGreycloud <rayGreycloud@gmail.com>
2017-02-05 11:58:10 -0800

    Add AgeRange and YearsActiveRange queries

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,25 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minAge = Artist
+    // All artists
+    .find({})
+    // Sort by age ascending
+    .sort({ age: 1 })
+    // Limit results to 1
+    .limit(1)
+    // Results still array, so pull off age property of first item
+    .then(artists => artists[0].age);
+
+  const maxAge = Artist
+    .find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  return Promise.all([minAge, maxAge])
+    // Create object with min and max values
+    .then((result) => {
+      return { min: result[0], max: result[1]};
+    });
 };
--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,27 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minYears = Artist
+    // All artists
+    .find({})
+    // Sort by age ascending
+    .sort({ yearsActive: 1 })
+    // Limit results to 1
+    .limit(1)
+    // Results still array, so pull off age property of first item
+    .then(artists => artists[0].yearsActive);
+
+  const maxYears = Artist
+    .find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+
+  return Promise.all([minYears, maxYears])
+    // Create object with min and max values
+    .then((result) => {
+      return { min: result[0], max: result[1]};
+    });
+
+
 };
