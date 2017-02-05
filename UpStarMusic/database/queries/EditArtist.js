b0d020368e1d6d12301b55dd8c3b968ac6d4efc6
rayGreycloud <rayGreycloud@gmail.com>
2017-02-05 07:23:11 -0800

    Refactor to preferred method update

--- UpStarMusic/database/queries/EditArtist.js
+++ UpStarMusic/database/queries/EditArtist.js
@@ -7,4 +7,7 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves when the record is edited
  */
 module.exports = (_id, artistProps) => {
+  return Artist.update({ _id }, artistProps);
+
+//  return Artist.findByIdAndUpdate(_id, artistProps);
 };
